5a4b48f97ed269c39bf5abd60a59d66ee95ded3d
sambot <sambottoni@gmail.com>
2019-03-30 09:34:02 -0500

    update ll and refresh cmder

--- user_aliases.cmd
+++ user_aliases.cmd
@@ -7,7 +7,7 @@
 e.=explorer .
 gl=git log --oneline --all --graph --decorate  $*
 ls=ls --show-control-chars -F --color $*
-ll=ls -l --show-control-chars -F --color $*
+ll=ls -l -a --show-control-chars -F --color $*
 pwd=cd
 clear=cls
 history=cat "%CMDER_ROOT%\config\.history"
